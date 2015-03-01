ec1ea7506828254a8acc7ca64681a9e71f2d6d85
Justin Loew <jloloew@gmail.com>
2015-03-01 09:15:29 -0600

    implemented adding, editing, and deleting reminder lists

--- Reminders/MasterViewController.swift
+++ Reminders/MasterViewController.swift
@@ -20,8 +20,10 @@ class MasterViewController: UITableViewController, UITextFieldDelegate {
     var hasCompletedReminders = true
 	
 	var eventStore: EKEventStore!
+	
+	var source: EKSource!
 
-    
+	
     override func awakeFromNib() {
         super.awakeFromNib()
         if UIDevice.currentDevice().userInterfaceIdiom == .Pad {
@@ -50,9 +52,26 @@ class MasterViewController: UITableViewController, UITextFieldDelegate {
 		self.eventStore = EKEventStore()
 		requestPermissionIfNecessary()
 		if hasPermission() {
+			setUpEventKit()
 			reloadReminders()
 		}
     }
+	
+	func setUpEventKit() {
+		// set up source
+		let sources = eventStore.sources() as! [EKSource]
+		for src in sources {
+			if src.sourceType.value == EKSourceTypeCalDAV.value {
+				// iCloud
+				self.source = src
+			} else if src.sourceType.value == EKSourceTypeLocal.value {
+				// fallback
+				if self.source == nil {
+					self.source = src
+				}
+			}
+		}
+	}
 	
 	func reloadReminders() {
 		// remove all existing reminder lists
@@ -108,6 +127,8 @@ class MasterViewController: UITableViewController, UITextFieldDelegate {
 	func requestPermissionIfNecessary() {
 		if !hasPermission() {
 			self.eventStore.requestAccessToEntityType(EKEntityTypeReminder) { (accessGranted, error) -> Void in
+				self.setUpEventKit()
+				
 				if error != nil {
 					println("Error while requesting calendar access: \(error)")
 				} else {
@@ -134,6 +155,16 @@ class MasterViewController: UITableViewController, UITextFieldDelegate {
 			let cell = self.tableView.cellForRowAtIndexPath(indexPath) as! ReminderListCell
 			if textField == cell.reminderListName {
 				return cell
+			}
+		}
+		return nil
+	}
+	
+	func calendarForName(name: String) -> EKCalendar! {
+		let calendars = source.calendarsForEntityType(EKEntityTypeReminder) as! Set<EKCalendar>
+		for cal in calendars {
+			if cal.title == name {
+				return cal
 			}
 		}
 		return nil
@@ -197,20 +228,32 @@ class MasterViewController: UITableViewController, UITextFieldDelegate {
     }
 	
 	override func tableView(tableView: UITableView, willBeginEditingRowAtIndexPath indexPath: NSIndexPath) {
-		let cell = tableView.cellForRowAtIndexPath(indexPath) as! ReminderListCell
-		cell.reminderListName.enabled = true
+		if let cell = tableView.cellForRowAtIndexPath(indexPath) as! ReminderListCell? {
+			cell.reminderListName.enabled = true
+		}
 	}
 	
 	override func tableView(tableView: UITableView, didEndEditingRowAtIndexPath indexPath: NSIndexPath) {
-		let cell = tableView.cellForRowAtIndexPath(indexPath) as! ReminderListCell
-		cell.reminderListName.enabled = false
-		//TODO: save
+		if let cell = tableView.cellForRowAtIndexPath(indexPath) as! ReminderListCell? {
+			cell.reminderListName.enabled = false
+		}
 	}
 
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+			let cell = tableView.cellForRowAtIndexPath(indexPath) as! ReminderListCell
+			// delete the calendar
+			let calendar = self.calendarForName(cell.reminderListName.text)
+			var error = NSErrorPointer()
+			self.eventStore.removeCalendar(calendar, commit: true, error: error)
+			if error != nil {
+				println("Error removing calendar: \(error)")
+			} else {
+				println("Successfully removed calendar.")
+			}
+			
             reminderLists.removeAtIndex(indexPath.row)
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
         }
@@ -225,19 +268,31 @@ class MasterViewController: UITableViewController, UITextFieldDelegate {
 	
 	/// Save the new Reminder List
 	func textFieldDidEndEditing(textField: UITextField) {
-		let cell = self.cellForTextField(textField)!
-		let calendar = EKCalendar(forEntityType: EKEntityTypeReminder, eventStore: self.eventStore)
-		calendar.title = textField.text
-		let color = cell.reminderListColor.textColor
-		calendar.CGColor = color.CGColor
+		if let cell = self.cellForTextField(textField) {
+			self.createReminderList(name: textField.text, color: cell.reminderListColor.textColor)
+		}
 	}
 	
 	func textFieldShouldReturn(textField: UITextField) -> Bool {
 		let cell = self.cellForTextField(textField)
 		cell?.setEditing(false, animated: true)
-		//TODO: save
 		return false
 	}
 	
+	func createReminderList(#name: String, color: UIColor) {
+		let calendar = EKCalendar(forEntityType: EKEntityTypeReminder, eventStore: self.eventStore)
+		calendar.title = name
+		calendar.CGColor = color.CGColor
+		calendar.source = self.source
+		
+		// save changes
+		var error = NSErrorPointer()
+		eventStore.saveCalendar(calendar, commit: true, error: error)
+		if error != nil {
+			println("Error creating new calendar: \(error)")
+		} else {
+			println("Successfully saved new calendar.")
+		}
+	}
+	
 }
-
